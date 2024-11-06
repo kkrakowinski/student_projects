1e63a28310c8bb5f46ea0d17cbf7646a7d879fc0
kkrakowinski <kkrakowinski_221i2@e.zseeim.edu.pl>
2024-11-06 11:20:51 +0000

    studentmachine send

---
+++ cpp/Tablica/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+int main(){
+int  t[2];
+t[0] = 22;
+t[1] = 37;
+for(int i=0; i<2; i++){
+	cout<<t[i]<<endl;
+
+int tt[4][4];
+tt[2][2] = 16;
+tt[3][3] = 5; 
+for(int y=0; y<4;y++){
+for(int x=0; x<4;x++){
+	cout <<tt[y] [x]<<endl; 
+}
+}
+return 0;
+} 
+
+}
