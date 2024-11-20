90b6e4f2dbab33a57e5029841ebd901d1e07a6db
kkrakowinski <student@student.machine>
2024-11-20 11:21:00 +0000

    studentmachine send

---
+++ cpp/kolkokrzyzyk/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+char t	[2][2];
+for(char  x=0;x<3;x++){
+	for(char y=0;y<3;y++){
+		t[x][y]='0';
+		cout<<t [x][y];
+		}
+		cout<<endl;
+	}
+return 0;
+}
